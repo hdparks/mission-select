099fc78317596b3fdeeeaef1625fb6ea62252c51
hparkinson <hparkinson@athlos.org>
2024-05-11 10:57:33 -0600

    add baseurl so assets still get loaded

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@ import { defineNuxtConfig } from "nuxt/config";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: "tilbuinn",
+  },
   devtools: { enabled: true },
   modules: ["@tresjs/nuxt",'@nuxtjs/tailwindcss'],
   css: ["~/assets/css/main.css"],
