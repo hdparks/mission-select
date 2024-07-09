ee87d01fd6640c5a4caacaa6b5cb740cb3a6f6d9
hparkinson <hparkinson@athlos.org>
2024-07-09 06:59:37 -0600

    add tailwind configuration for condensed aurebesh font

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ export default {
   theme: {
     extend: {},
     fontFamily: {
-      'aurebesh': ['Aurebesh']
+      'aurebesh': ['Aurebesh'],
+      'aurebeshCondensed': ['AurebeshCondensed']
     }
   },
   plugins: [],
